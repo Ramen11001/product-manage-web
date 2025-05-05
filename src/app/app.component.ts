98286b6e428a1cb297a43cbdd6727af1fa5caf0d
Solans <solanshnd@gmail.com>
2025-05-05 11:40:27 -0400

    [FTR] start with migration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule],
 })
 export class AppComponent {
   title = 'product-manage-web';
+  selection = { value: 'default' };
 }
