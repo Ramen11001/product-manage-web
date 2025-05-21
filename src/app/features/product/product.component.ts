3bdb40f8ab8b6b470255b7b6122cb2e59b358ec4
Ramen11001 <soltashnd@gmail.com>
2025-05-21 17:26:59 +0200

    [FTR] Implementing ProductComponent with Filters and Pagination

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,26 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { FormControl,} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+
+interface Producto {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-product',
   standalone: true,
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule,],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
-  /**
-   * Initializes ProductComponent.
-   *
-   * @constructor
-   * @param {Router} router - Manages route navigation.
-   */
+  productos: Producto[] = [];
+  nombreFiltro: string = '';
+  minPrecio: number | null = null;
+  maxPrecio: number | null = null;
+  paginaActual: number = 1;
+  totalPaginas: number = 1;
+
   constructor(
     private router: Router,
-  ) {
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
+  obtenerProductos(): void {
+    const params: any = {
+      search: this.nombreFiltro,
+      minPrecio: this.minPrecio,
+      maxPrecio: this.maxPrecio,
+      pagina: this.paginaActual
+    };
+
+    this.http.get<{ productos: Producto[], totalPaginas: number }>(`${environment.baseUrl}/products`, { params })
+      .subscribe(response => {
+        this.productos = response.productos;
+        this.totalPaginas = response.totalPaginas;
+      });
   }
 
-  
+  cambiarPagina(nuevaPagina: number): void {
+    this.paginaActual = nuevaPagina;
+    this.obtenerProductos();
+  }
 }
